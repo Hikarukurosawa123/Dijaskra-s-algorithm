f4ec005de7f12df0ef41227b09750e9f9bfe7ab0
Hikarukurosawa123 <94869114+Hikarukurosawa123@users.noreply.github.com>
2022-05-13 10:51:28 +0900

    with_interface

--- lab05/test5.c
+++ lab05/test5.c
@@ -1,9 +1,8 @@
 #include "lab5.h"
 
-int main(){
-    //Building the graph in Figure 1
-    Graph *gr = initialize_graph();
-        add(gr, "Spadina");
+void example(Graph* gr)
+{
+    add(gr, "Spadina");
 
     add(gr, "Kipling");
     add(gr, "Yorkdale");
@@ -35,13 +34,11 @@ int main(){
     update(gr, "Sheppard-Yonge", "Finch", 3);
     update(gr, "Sheppard-Yonge", "Don Mills", 6);
 
-    print_gr(gr);
-    //disrupt(gr, "Bay");
-    print_gr(gr);
-
+   // print_gr(gr);
 
     
     char **r = plan_route(gr, "Kipling", "Don Mills");
+    print_route(r); 
     /*
     print_route(r, "Kipling");
 
@@ -57,8 +54,97 @@ int main(){
     print_gr(gr);
     r = plan_route(gr, "Kipling", "Don Mills");
     print_route(r, "Kipling");
-    
-    free_gr(gr);
     */
+   //free_gr(gr);
+    
+}
+
+int main(){
+    //Building the graph in Figure 1
+    Graph *gr = initialize_graph();
+    char input;
+    printf("Input an integer that corresponds to the desired action: \n");
+    printf("1. add station \n2. update station (add connection between the stations with speicified weightings \n3. delete a station \n4. print current stations and connections \n5. find route \n6. run example\n7. exit");
+    //scanf("%c", &input);
+            char* station = malloc(sizeof(char)*MAX_LEN);
+            char* start = malloc(sizeof(char)*MAX_LEN);
+            char* dest = malloc(sizeof(char)*MAX_LEN);
+            int weight; 
+            char **r;
+
+    while(1)
+    {
+        scanf("%c", &input);
+
+        switch(input)
+    {
+        case '1':
+            printf("enter station name");
+            scanf("%s", station);
+            add(gr, station);
+            printf("continue? Press Y or N");
+            break;
+        case '2':
+        
+            printf("enter starting station");
+            scanf("%s", start);
+            printf("enter destination station");
+            scanf("%s", dest);
+            printf("enter weighting");
+            scanf("%d", &weight);
+            update(gr, start, dest, weight);
+            printf("continue? Press Y or N");
+
+            
+            break;
+        case '3':
+            printf("enter name of the deleting station");
+            scanf("%s", station);
+            disrupt(gr, station);
+            printf("continue? Press Y or N");
+
+            break;
+        case '4':
+            print_gr(gr);
+            printf("continue? Press Y or N");
+
+            break;
+        case '5':
+            printf("enter starting station");
+            scanf("%s", start);
+            printf("enter destination station");
+            scanf("%s", dest);
+            r = plan_route(gr, start, dest);
+            print_route(r);
+            printf("continue? Press Y or N");
+
+            break;
+        case '6':
+            example(gr);
+            printf("continue? Press Y or N");
+
+            break;
+        case '7'://exit
+            break;
+        case 'N':
+            break;
+        case 'Y':
+            printf("1. add station \n2. update station (add connection between the stations with speicified weightings \n3. delete a station \n4. print current stations and connections \n5. find route \n6. run example\n7. exit");
+
+            break;
+        
+        
+    }
+
+    if(input == 'N' || input == '7')
+    {
+        break;
+    }
+
+    }
+
+
+    
+        
     return 0;
 }
