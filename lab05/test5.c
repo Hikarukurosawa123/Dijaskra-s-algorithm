67a309efa34144d4df4eb0e9d39ccfc9923c6bc4
Hikarukurosawa123 <94869114+Hikarukurosawa123@users.noreply.github.com>
2022-05-13 09:02:48 +0900

    Add files via upload

---
+++ lab05/test5.c
@@ -0,0 +1,64 @@
+#include "lab5.h"
+
+int main(){
+    //Building the graph in Figure 1
+    Graph *gr = initialize_graph();
+        add(gr, "Spadina");
+
+    add(gr, "Kipling");
+    add(gr, "Yorkdale");
+    add(gr,"Bay");
+    
+    add(gr,"Union");
+    
+    add(gr,"Bloor-Yonge");
+    
+    add(gr,"Kennedy");
+    add(gr,"Sheppard-Yonge");
+    add(gr,"Finch");
+    add(gr,"Don Mills");
+
+
+
+    
+
+    update(gr, "Kipling", "Spadina", 12);
+
+    update(gr, "Spadina", "Yorkdale", 8);
+    update(gr, "Spadina", "Bay", 2);
+    update(gr, "Spadina", "Union", 4);
+
+    update(gr, "Bay", "Bloor-Yonge", 1);
+    update(gr, "Union", "Bloor-Yonge", 3);
+    update(gr, "Bloor-Yonge", "Kennedy", 14);
+    update(gr, "Bloor-Yonge", "Sheppard-Yonge", 11);
+    update(gr, "Sheppard-Yonge", "Finch", 3);
+    update(gr, "Sheppard-Yonge", "Don Mills", 6);
+
+    print_gr(gr);
+    //disrupt(gr, "Bay");
+    print_gr(gr);
+
+
+    
+    char **r = plan_route(gr, "Kipling", "Don Mills");
+    /*
+    print_route(r, "Kipling");
+
+    //Bye bye Bay!
+    disrupt(gr, "Bay");
+    print_gr(gr);
+    r = plan_route(gr, "Kipling", "Don Mills");
+    print_route(r, "Kipling");
+
+    //Hello new stations
+    update(gr, "Yorkdale", "Sheppard West", 5);
+    update(gr, "Sheppard West", "Sheppard-Yonge", 4);
+    print_gr(gr);
+    r = plan_route(gr, "Kipling", "Don Mills");
+    print_route(r, "Kipling");
+    
+    free_gr(gr);
+    */
+    return 0;
+}
