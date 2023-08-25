11c34d22be353480c3a0a426b1d53510fe9f684f
hikaruk <94869114+Hikarukurosawa123@users.noreply.github.com>
2023-08-25 16:48:57 -0400

    Delete MATLAB/MAT 292 ODE directory

--- MATLAB/MAT 292 ODE/LAB 3/euler2.m
+++
@@ -1,8 +0,0 @@
-
-function y = euler2(f_prime, t0, tN, y0, h)
-t = linspace(t0, tN, (tN-t0)/h+1)
-y = zeros(size(t));
-y(1) = y0; 
-for i = 2: size(t,2)
-    y(i) = y(i-1) + f_prime(t(i-1), y(i-1))*h;
-end
